cba60ff9f8aaeb0bf63d03c7beec6cd4c58fc832
Ma <101021254+CodeWithMa@users.noreply.github.com>
2024-08-27 15:20:16 +0200

    Add font.png and use it in title_screen to draw menu text instead of having the text hardcoded in title_screen.png

--- Makefile
+++ Makefile
@@ -8,22 +8,26 @@ RGBASM_WARN = -Weverything -Wnumeric-string=2 -Wtruncation=1
 
 # Define files and settings
 GB_PALETTE = "\#FFFFFF,\#cfcfcf,\#686868,\#000000;"
-OUTPUT_GB = schlange.gb
+ROM_NAME = schlange
 
 # Define object files and other intermediates
 OBJ_FILES = main.o input.o title_screen.o util/memory.o util/vblank.o
 GFX_BACKGROUND_TILES = gfx/background.2bpp
 GFX_BACKGROUND_TILEMAP = gfx/background.tilemap
+GFX_FONT_TILES = gfx/font.2bpp
 GFX_TITLE_SCREEN_TILES = gfx/title_screen.2bpp
 GFX_TITLE_SCREEN_TILEMAP = gfx/title_screen.tilemap
 GFX_SNAKE_HEAD = gfx/snake_head.2bpp
 
 # Default target
-all: $(OUTPUT_GB)
+all: $(ROM_NAME).gb
 
 # Convert graphics to 2bpp format
 $(GFX_BACKGROUND_TILES): gfx/background.png
 	$(RGBGFX) -c $(GB_PALETTE) -o $(GFX_BACKGROUND_TILES) --unique-tiles $<
+
+$(GFX_FONT_TILES): gfx/font.png
+	$(RGBGFX) -c $(GB_PALETTE) -o $(GFX_FONT_TILES) --unique-tiles $<
 
 $(GFX_TITLE_SCREEN_TILES): gfx/title_screen.png
 	$(RGBGFX) -c $(GB_PALETTE) -o $(GFX_TITLE_SCREEN_TILES) --tilemap $(GFX_TITLE_SCREEN_TILEMAP) --unique-tiles $<
@@ -34,7 +38,7 @@ $(GFX_SNAKE_HEAD): gfx/snake_head.png
 input.o: input.asm
 	$(RGBASM) $(RGBASM_WARN) -o $@ $<
 
-title_screen.o: title_screen.asm $(GFX_TITLE_SCREEN_TILES) $(GFX_TITLE_SCREEN_TILEMAP)
+title_screen.o: title_screen.asm $(GFX_FONT_TILES) $(GFX_TITLE_SCREEN_TILES) $(GFX_TITLE_SCREEN_TILEMAP)
 	$(RGBASM) $(RGBASM_WARN) -o $@ $<
 
 util/memory.o: util/memory.asm
@@ -48,12 +52,12 @@ main.o: main.asm $(GFX_BACKGROUND_TILES) $(GFX_BACKGROUND_TILEMAP) $(GFX_SNAKE_H
 	$(RGBASM) $(RGBASM_WARN) -o $@ $<
 
 # Link object file to create the ROM
-$(OUTPUT_GB): $(OBJ_FILES)
-	$(RGBLINK) -o $(OUTPUT_GB) $(OBJ_FILES)
-	$(RGBFIX) -t "SCHLANGE" -v -p 0xFF $(OUTPUT_GB)
+$(ROM_NAME).gb: $(OBJ_FILES)
+	$(RGBLINK) -n $(ROM_NAME).sym -m $(ROM_NAME).map -o $(ROM_NAME).gb $(OBJ_FILES)
+	$(RGBFIX) -t "SCHLANGE" -v -p 0xFF $(ROM_NAME).gb
 
 # Clean up build artifacts
 clean:
-	rm -f $(OBJ_FILES) $(OUTPUT_GB) $(GFX_BACKGROUND_TILES) $(GFX_TITLE_SCREEN_TILES) $(GFX_TITLE_SCREEN_TILEMAP) $(GFX_SNAKE_HEAD)
+	rm -f $(OBJ_FILES) $(ROM_NAME).gb $(GFX_BACKGROUND_TILES) $(GFX_FONT_TILES) $(GFX_TITLE_SCREEN_TILES) $(GFX_TITLE_SCREEN_TILEMAP) $(GFX_SNAKE_HEAD)
 
 .PHONY: all clean
--- title_screen.asm
+++ title_screen.asm
@@ -1,22 +1,25 @@
 INCLUDE "hardware.inc"
 
+DEF FIRST_MENU_TEXT_START_ADDRESS EQU $9928
+DEF ROW_SIZE EQU $20
+
 SECTION "Title Screen", ROM0
 
 ShowTitleScreen::
 
-LoadTitleScreen:
+    call LoadTitleScreenTiles
+    call LoadFontTiles
+    call LoadTitleScreenTilemap
 
-    ; Load title screen tiles
-    ld de, TitleScreenTiles
-    ld hl, _VRAM9000
-    ld bc, TitleScreenTilesEnd - TitleScreenTiles
-    call Memcopy
+    ; Write menu text to background
+    ld de, FIRST_MENU_TEXT_START_ADDRESS
+    ld hl, StartText
+    call DrawTextTiles
 
-    ; Load title screen tilemap
-    ld de, TitleScreenTilemap
-    ld hl, _SCRN0
-    ld bc, TitleScreenTilemapEnd - TitleScreenTilemap
-    call Memcopy
+    ; Write second menu text to background
+    ld de, FIRST_MENU_TEXT_START_ADDRESS + ROW_SIZE
+    ld hl, TodoText
+    call DrawTextTiles
 
     ; Put in function?
     ; Turn the LCD on
@@ -42,6 +45,7 @@ WaitInTitleScreen:
     cp 144
     jp nc, WaitInTitleScreen
 
+    ; TODO Do I need this wait?
     call WaitVBlank
 
     call UpdateKeys
@@ -53,8 +57,67 @@ WaitInTitleScreen:
 
     jp WaitInTitleScreen
 
+LoadTitleScreenTiles:
+    ld de, TitleScreenTiles
+    ld hl, _VRAM9000
+    ld bc, TITLE_SCREEN_TILES_SIZE
+    call Memcopy
+    ret
+
+LoadFontTiles:
+    ld de, FontTiles
+    ld hl, _VRAM9000 + TITLE_SCREEN_TILES_SIZE
+    ld bc, FONT_TILES_SIZE
+    call Memcopy
+    ret
+
+LoadTitleScreenTilemap:
+    ld de, TitleScreenTilemap
+    ld hl, _SCRN0
+    ld bc, TitleScreenTilemapEnd - TitleScreenTilemap
+    call Memcopy
+    ret
+
+; Draws text to the background
+; @param de: Address to start drawing the tiles on
+; @param hl: Address to the start of the text to draw
+DrawTextTiles:
+    ; Stop at the end of the string
+    ld a, [hli]
+    cp 255
+    ret z
+
+    ld [de], a
+    inc de
+
+    jp DrawTextTiles
+
 TitleScreenTiles: INCBIN "gfx/title_screen.2bpp"
 TitleScreenTilesEnd:
+DEF TITLE_SCREEN_TILES_SIZE EQU TitleScreenTilesEnd - TitleScreenTiles
+DEF TITLE_SCREEN_NUMBER_OF_TILES EQU TITLE_SCREEN_TILES_SIZE / 16
 
 TitleScreenTilemap: INCBIN "gfx/title_screen.tilemap"
 TitleScreenTilemapEnd:
+
+; TODO create extra asm file for font related code?
+FontTiles: INCBIN "gfx/font.2bpp"
+FontTilesEnd:
+DEF FONT_TILES_SIZE EQU FontTilesEnd - FontTiles
+DEF FONT_NUMBER_OF_TILES EQU FONT_TILES_SIZE / 16
+STATIC_ASSERT FONT_NUMBER_OF_TILES == 34, "Number of font tiles is wrong!"
+STATIC_ASSERT TITLE_SCREEN_NUMBER_OF_TILES + FONT_NUMBER_OF_TILES < 129, "Number of total background tiles is too large!"
+
+CHARMAP "A", TITLE_SCREEN_NUMBER_OF_TILES + 0
+CHARMAP "B", TITLE_SCREEN_NUMBER_OF_TILES + 1
+CHARMAP "C", TITLE_SCREEN_NUMBER_OF_TILES + 2
+CHARMAP "D", TITLE_SCREEN_NUMBER_OF_TILES + 3
+CHARMAP "O", TITLE_SCREEN_NUMBER_OF_TILES + 14
+CHARMAP "P", TITLE_SCREEN_NUMBER_OF_TILES + 15
+CHARMAP "Q", TITLE_SCREEN_NUMBER_OF_TILES + 16
+CHARMAP "R", TITLE_SCREEN_NUMBER_OF_TILES + 17
+CHARMAP "S", TITLE_SCREEN_NUMBER_OF_TILES + 18
+CHARMAP "T", TITLE_SCREEN_NUMBER_OF_TILES + 19
+
+StartText: db "START", 255
+TodoText: db "TODO", 255
